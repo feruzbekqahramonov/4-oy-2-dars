d24f2409fab5e7aef2d5df780e311c8f7522bf04
feruzbekqahramov <feqahramnov@mail.com>
2023-11-26 16:27:33 +0500

    hom finish

---
+++ js/script.js
@@ -0,0 +1,133 @@
+// 1-masala
+// let misol1 = +prompt ("misol 1")
+// let misol2 = +prompt ("misol 2")
+
+// if (m1a > misol2) {
+//     misol2**=2
+//     console.log (m1a)
+// }
+// else {
+//     misol2**=2
+//     console.log (misol2)
+// }
+
+// 2-masala 
+// let misol1 = +prompt ("misol a kriting")
+// let misol2 = +prompt ("misol b kiriting")
+// let misol3 = +prompt ("misol c kiriting")
+
+// if (m2a > m2b) {
+//     // console.log (misol1)
+//     if (misol1 > misol2) {
+//         console.log (misol1)
+//     }
+    
+//     else {
+//         console.log (misol3)
+//     }
+// }
+// else {
+//     // console.log (misol2)
+//     if (misol2 > misol3) {
+//         console.log (misol2)
+//     }
+    
+//     else {
+//         console.log (misol3)
+//     }
+// }
+
+// 3-masala
+// let misol1 = +prompt ("misol a kriting");
+
+// if (misol1 % 2 == 0) {
+//     console.log ("Siz kiritgan son: Juft")
+// }
+// else {
+//     console.log ("Siz kiritgan son: Toq")
+// }
+
+// 4-masala 
+// let misol1 = +prompt ("a sonini kiriting");
+
+// if (misol1 % 5 == 0) {
+//     console.log ("Rost")
+// }
+// else {
+//     console.log ("Yolgon")
+// }
+
+// 5-masala
+//  let misol5 = +prompt ("son kiriting");
+
+//  if (misol5 % 10 == 0) {
+//      console.log ("Rost")
+//  }
+//  else {
+//      console.log ("Yolg`on")
+// }
+
+// 6-masala 
+// let misol6 = +prompt ("son kiriting")
+
+// if (misol6 % 1 == 0) {
+//     console.log ("Rost")
+// }
+// else {
+//     console.log ("Yolg'on")
+// }
+
+// 7-masala 
+// let misol7 = +prompt ("son kiriting!!!")
+
+// if (misol7 % 10 == 0, misol7 % 1 == 0) {
+//     console.log ("Rost")
+// }
+// else {
+//     console.log ("Yolg'on")
+// }
+
+// 8-masala
+// let misol8 = +prompt ("ihtiyoriy son kiriting!!!");
+
+// misol8=misol8%10;
+// if (misol8 % 2 == 0) {
+//     console.log ("Rost");
+// }else {
+//     console.log ("Yolg'on")
+// }
+
+// 9-masala
+// let misol9 = +prompt ("tasodifiy son kiriting");
+
+// if (misol9 < 1000) {
+//     misol9 = misol9/100;
+// console.log (misol9);
+// }else {
+//     console.log ("Siz kiritgan don 3 xonali")
+// }
+
+// 10-masala
+//     let misol1 = +prompt ("son kiriting");
+// let misol2;
+// let misol3;
+// misol2 = misol1 / 100
+// misol3 = misol1 %100 / 10
+// misol1 = misol1 % 10
+
+// if (misol1 > misol2){
+//     // console.log (misol1)
+//     if (misol1 > misol3) {
+//         console.log (misol1)
+//     }else {
+//         console.log (misol3)
+//     }
+// }
+// else{
+//     // console.log (misol2)
+//     if (misol2 > misol3) {
+//         console.log (misol2)
+//     }else {
+//         console.log (misol3)
+//     }
+// }
